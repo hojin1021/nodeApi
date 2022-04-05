f1d597c3725f72496a79225fcfe2d32f379e6efd
hojin1021 <hojin0921@hanmail.net>
2022-04-06 00:28:54 +0900

    api

--- src/router/indexRouter.js
+++ src/router/indexRouter.js
@@ -1,7 +1,7 @@
 const indexController = require("../controller/indexConroller");
 
 exports.indexRouter = function (app) {
-  app.get("/students", indexController.readStudents);
-  app.post("/student", indexController.createStudents);
-  app.get("/lectures", indexController.readLectures);
+  app.get("/api/students", indexController.readStudents);
+  app.post("/api/student", indexController.createStudents);
+  app.get("/api/lectures", indexController.readLectures);
 };
